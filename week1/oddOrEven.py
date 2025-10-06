51bd2124e65cc1091c9d6b3c89200652cd8ba1cc
Emmanuel Owusu-Ampaw <118029142+emmanueloa503@users.noreply.github.com>
2025-10-06 03:00:26 -0400

    Delete week1 directory

--- week1/oddOrEven.py
+++
@@ -1,8 +0,0 @@
-#Write a program that iterates from 1 to 20, printing each number and whether it's odd or even.
-
-for numbers in range(0, 21):
-    if numbers % 2 ==0 :
-        print(numbers, 'is even')
-        
-    else:
-        print(numbers , 'is odd')
--- week1/palindrome.py
+++
@@ -1,9 +0,0 @@
-#Write a program to check if a string is a palindrome or not.
-
-print("Enter word: ")
-word = input()
-
-if word == word[::-1]:
-    print(word, 'is a palindrome')
-else:
-     print(word, 'is not a palindrome')
