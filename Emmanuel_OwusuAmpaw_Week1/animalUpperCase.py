83095aa999d1468b9d3c27b8d34674205e98cff3
Emmanuel Owusu-Ampaw <118029142+emmanueloa503@users.noreply.github.com>
2025-10-06 03:02:00 -0400

    Add files via upload

---
+++ Emmanuel_OwusuAmpaw_Week1/animalUpperCase.py
@@ -0,0 +1,9 @@
+#Iterate through the following list of animals and print each one in all caps.
+
+#animalUpperCase.py
+
+animals = ['tiger', 'elephant', 'monkey', 'zebra', 'panther']
+
+uppercase_animals = [word.upper() for word in animals]
+
+print(uppercase_animals)
